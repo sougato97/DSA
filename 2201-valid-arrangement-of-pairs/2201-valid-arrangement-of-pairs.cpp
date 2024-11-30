d431e6bcd63a97778c2d79db559b68eea59f86aa
Sougato Bagchi <90167287+sougato97@users.noreply.github.com>
2024-11-29 19:45:37 -0800

    Time: 241 ms (100%), Space: 233.1 MB (96.61%) - LeetHub

--- 2201-valid-arrangement-of-pairs/2201-valid-arrangement-of-pairs.cpp
+++ 2201-valid-arrangement-of-pairs/2201-valid-arrangement-of-pairs.cpp
@@ -1,61 +1,49 @@
 class Solution {
 public:
     vector<vector<int>> validArrangement(vector<vector<int>>& pairs) {
-        unordered_map<int, deque<int>> adjacencyMatrix;
-        unordered_map<int, int> inDegree, outDegree;
-
-        // Build the adjacency list and track in-degrees and out-degrees
+        unordered_map<int, vector<int>> adjacencyList;
+        unordered_map<int, int> inOutDegree;
+        
+        // Build graph and count in/out degrees
         for (const auto& pair : pairs) {
-            int start = pair[0], end = pair[1];
-            adjacencyMatrix[start].push_back(end);
-            outDegree[start]++;
-            inDegree[end]++;
+            adjacencyList[pair[0]].push_back(pair[1]);
+            inOutDegree[pair[0]]++;  // out-degree
+            inOutDegree[pair[1]]--;  // in-degree
         }
-
-        vector<int> result;
-
-        // Find the start node (outDegree == inDegree + 1)
-        int startNode = -1;
-        for (const auto& entry : outDegree) {
-            int node = entry.first;
-            if (outDegree[node] == inDegree[node] + 1) {
+        
+        // Find starting node (head)
+        int startNode = pairs[0][0];
+        for (const auto& [node, degree] : inOutDegree) {
+            if (degree == 1) {
                 startNode = node;
                 break;
             }
         }
-
-        // If no such node exists, start from the first pair's first element
-        if (startNode == -1) {
-            startNode = pairs[0][0];
-        }
-
+        
+        vector<int> path;
         stack<int> nodeStack;
         nodeStack.push(startNode);
-
-        // Iterative DFS using stack
+        
         while (!nodeStack.empty()) {
-            int node = nodeStack.top();
-            if (!adjacencyMatrix[node].empty()) {
-                // Visit the next node
-                int nextNode = adjacencyMatrix[node].front();
-                adjacencyMatrix[node].pop_front();
-                nodeStack.push(nextNode);
-            } else {
-                // No more neighbors to visit, add node to result
-                result.push_back(node);
+            auto& neighbors = adjacencyList[nodeStack.top()];
+            if (neighbors.empty()) {
+                path.push_back(nodeStack.top());
                 nodeStack.pop();
+            } else {
+                int nextNode = neighbors.back();
+                nodeStack.push(nextNode);
+                neighbors.pop_back();
             }
         }
-
-        // Reverse the result since we collected nodes in reverse order
-        reverse(result.begin(), result.end());
-
-        // Construct the result pairs
-        vector<vector<int>> pairedResult;
-        for (int i = 1; i < result.size(); ++i) {
-            pairedResult.push_back({result[i - 1], result[i]});
+        
+        vector<vector<int>> arrangement;
+        int pathSize = path.size();
+        arrangement.reserve(pathSize - 1);
+        
+        for (int i = pathSize - 1; i > 0; --i) {
+            arrangement.push_back({path[i], path[i-1]});
         }
-
-        return pairedResult;
+        
+        return arrangement;
     }
 };
