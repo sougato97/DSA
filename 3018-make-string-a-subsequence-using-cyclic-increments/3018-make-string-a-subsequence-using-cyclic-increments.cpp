d8b3c932e3cbfb341f12355f448f295e5cbecc63
Sougato Bagchi <90167287+sougato97@users.noreply.github.com>
2024-12-04 15:51:44 -0800

    Time: 8 ms (26.78%), Space: 16.9 MB (27.62%) - LeetHub

--- 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.cpp
+++ 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     bool canMakeSubsequence(string str1, string str2) {
-        int str2Index = 0;
-        int lengthStr1 = str1.size(), lengthStr2 = str2.size();
-
-        // Traverse through both strings using a for loop
-        for (int str1Index = 0;
-             str1Index < lengthStr1 && str2Index < lengthStr2; ++str1Index) {
-            // Check if characters match, or if str1[str1Index] can be
-            // incremented to str2[str2Index]
-            if (str1[str1Index] == str2[str2Index] ||
-                (str1[str1Index] + 1 == str2[str2Index]) ||
-                (str1[str1Index] - 25 == str2[str2Index])) {
-                // If match found, move to next character in str2
-                str2Index++;
+        int i = 0; // str1 iterator
+        int j = 0; // str2 iterator
+        while (i<str1.size() && j < str2.size()){
+            if(str1[i] == str2[j] || str1[i] + 1 == str2[j] || str1[i] - 25 == str2[j]){
+                j++;
+                i++;
             }
+            else
+                i++;
         }
-        // Check if all characters in str2 were matched
-        return str2Index == lengthStr2;
+        if (j == str2.size()){
+            return true;
+        }
+        return false;
     }
 };
