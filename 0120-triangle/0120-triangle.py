f3cf3bae53b79f9e25808fa3b43498085f575cbc
Sougato Bagchi <90167287+sougato97@users.noreply.github.com>
2025-01-13 14:02:46 -0500

    Time: 10 ms (21.45%), Space: 19.5 MB (20.54%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,19 +1,21 @@
 class Solution:
-    def solveMem(self, triangle, row, col, dp):
+    def memoization(self, triangle, row, col, dp):
         if (row >= len(triangle)):
             return 0
         if (len(triangle) == 1):
             return triangle[row][col]
-        # for getting previously calculated values
         if (dp[row][col] != -1):
             return dp[row][col]
-        
-        # caluclate the dp[row][col] recursively
-        dp[row][col] = triangle[row][col] + min(self.solveMem(triangle, row+1, col, dp), 
-                                                self.solveMem(triangle, row+1, col+1, dp))
-        return dp[row][col]
+        # calculate the dp[row][col] recursively and store it
+        dp[row][col] = triangle[row][col] + min(self.memoization(triangle, row + 1, col, dp),
+                                                self.memoization(triangle, row + 1, col + 1, dp))
 
+        return dp[row][col]
+        
+    
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        # Initialize a 2d list of same size, with value -1
-        dp = [[-1 for _ in range(len(triangle))] for _ in range(len(triangle))]
-        return self.solveMem(triangle, 0, 0, dp)
+        # create a list of same size, also initializing it with -1
+        dp = [[-1 for _ in range(len(triangle))] 
+                for _ in range(len(triangle))]
+        # function call arguments {OG_LIST, ROW, COL, DP_LIST}
+        return self.memoization(triangle, 0, 0, dp)
